e1f38a6a54338b7e258baeb28cfd4e52422defe4
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-05-13 20:57:32 -0700

    moved testing codes

--- BST.py
+++ BST.py
@@ -1,4 +1,5 @@
-#* Part 1: Create a BSTNode class
+
+# * Part 1: Create a BSTNode class
 class BSTNode:
   def __init__(self, data=None, left=None, right=None):
     self.data = data
@@ -12,40 +13,6 @@ class BSTNode:
   def __repr__(self):
     return str(self.data)
 
-
-# TODO Part 2: Create a BST class
-class BST:
-  def __init__(self, root=None):
-    self.root = root
-    self.content = []
-    
-    # To allow the nodes to be printed
-  def __str__(self):
-    if self.root == None:
-      return "The tree is empty"
-    else:
-      self.output = ''
-      self.print_tree(node=self.root)
-      return self.output
-  
-  def __repr__(self):
-    if self.root == None:
-      return "The tree is empty"
-    else:
-      self.output = ''
-      self.print_tree(node=self.root)
-      return self.output
-    
-  def print_tree(self, node, level=0):
-    if node != None:
-      self.print_tree(node.right, level + 1)
-      self.output += ' ' * 4 * level + '-> ' + str(node.data) + ''
-      self.print_tree(node.left, level + 1)
-
-
-# TODO Part 3: Add functionality to your BST class
-
-
 #* Testing code part1 Writing a BSTNode Class
 node1 = BSTNode(3)
 print(node1)  # 3
@@ -58,6 +25,39 @@ print(node3)  # None
 node3.data = 5
 print(node3)  # 5
 
+# * Part 2: Create a BST class
+class BST:
+  def __init__(self, root=None):
+    self.root = root
+    self.content = []
+    
+  def __str__(self):
+    # If the tree is empty, return a message indicating that
+    if self.root == None:
+      return "The tree is empty"
+    else:
+      self.output = ''
+      self.print_tree(node=self.root)
+      return self.output
+  
+  def __repr__(self):
+    # If the tree is empty, return a message indicating that
+    if self.root == None:
+      return "The tree is empty"
+    else:
+      self.output = ''
+      self.print_tree(node=self.root)
+      return self.output
+    
+  def print_tree(self, node, level=0):
+    if node != None:
+      self.print_tree(node.right, level + 1)  # Recursively traverse the right subtree
+      
+      # Add the current node's data to the output with proper indentation
+      self.output += ' ' * 4 * level + '-> ' + str(node.data) + ''
+      
+      self.print_tree(node.left, level + 1)  # Recursively traverse the left subtree
+
 
 #* Testing code part2 Writing a BST Class
 bst = BST()
@@ -69,3 +69,6 @@ print(bst)  # -> 4
 node2.right = node3
 print(bst)  #   -> 5
             # -> 4
+
+# TODO Part 3: Add functionality to your BST class
+
